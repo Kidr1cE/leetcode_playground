3a22b4a41b792ede34c2a7643cbe2b6c582c9a1d
Kidr1cE <kang89963@163.com>
2023-11-06 08:48:06 +0800

    ADD-Problem-318

---
+++ leetcode/maximum-product-of-word-lengths.go
@@ -0,0 +1,24 @@
+/*
+Date: 2023-11-6
+ProblemID: 318
+ProblemName: 最大单词长度乘积
+*/
+package leetcode
+
+func maxProduct(words []string) (ans int) {
+	masks := make([]int, len(words))
+	for i, word := range words {
+		for _, ch := range word {
+			masks[i] |= 1 << (ch - 'a')
+		}
+	}
+
+	for i, x := range masks {
+		for j, y := range masks[:i] {
+			if x&y == 0 && len(words[i])*len(words[j]) > ans {
+				ans = len(words[i]) * len(words[j])
+			}
+		}
+	}
+	return
+}
