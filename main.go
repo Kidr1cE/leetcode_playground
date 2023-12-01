02a88632ae36f5d9328e8e00127f9e6865a75199
Kidr1cE <kang89963@163.com>
2023-12-01 12:59:07 +0800

    fix:main.go

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"playground/leetcode"
 )
 
 func main() {
-	ints := []int{238838724, 196963851, 539418658, 120132686, 273213807, 57187185, 68854249, 619718192}
-	res := leetcode.MaxKelements(ints, 7)
-	fmt.Println(res)
+	fmt.Println("Hello world!")
 }
