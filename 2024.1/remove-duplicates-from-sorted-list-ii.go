e6ac64ff2166136312cfd086818029f047808391
Kidr1cE <kang89963@gmail.com>
2024-01-15 10:22:16 +0800

    ADD:Problem-82

---
+++ 2024.1/remove-duplicates-from-sorted-list-ii.go
@@ -0,0 +1,36 @@
+/*
+Date: 2023-12-15
+ProblemID: 82
+ProblemName: 删除排序链表中的重复元素 II
+*/
+
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	dummy := &ListNode{0, head}
+
+	cur := dummy
+	for cur.Next != nil && cur.Next.Next != nil {
+		if cur.Next.Val == cur.Next.Next.Val {
+			x := cur.Next.Val
+			for cur.Next != nil && cur.Next.Val == x {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return dummy.Next
+}
